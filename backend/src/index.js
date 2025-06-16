e836081d3ad27cfe60d00fd808e15dd1effbd3ba
Shiva vignesh kannan.K <135090729+Shivavigneshkannan-k@users.noreply.github.com>
2025-06-16 13:02:31 +0530

    configuring backend for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://employee-management-system-gbq4.onrender.com/"],
     credentials: true
   })
 );
